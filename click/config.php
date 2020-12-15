89531f06442c48ded1b8ccd021e49c5bc7b1193e
halfwit <michaelmisch1985@gmail.com>
2020-12-15 00:31:30 +0000

    Remove unused git and apt-utils fetch, clean up confusing comments

--- click/config.php
+++ click/config.php
@@ -1,7 +1,5 @@
 <?php
 
-// Copy this file to "config.php" and change the following values
-
 define("MYSQL_HOST", db);
 define("MYSQL_USER", getenv('DB_USERNAME'));
 define("MYSQL_PASSWORD", getenv('DB_PASSWORD'));

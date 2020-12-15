c164f80abb51b81b751f6d7f16ba4257da993e5a
halfwit <michaelmisch1985@gmail.com>
2020-12-15 00:44:16 +0000

    Initial commit for dockerizing danger

---
+++ danger/README.md
@@ -0,0 +1,4 @@
+## Running with docker
+
+1. Create **.env** based on the `.env.example` file
+2. Run ```docker-compose up```
---
+++ danger/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Copy this file to "config.php" and change the following values
+
+define("MYSQL_HOST", "db"); 
+define("MYSQL_USER", getenv('DB_USERNAME'));
+define("MYSQL_PASSWORD", getenv('DB_PASSWORD'));
+define("MYSQL_DATABASE", getenv('DB_DATABASE'));
+
+?>

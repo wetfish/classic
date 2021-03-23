574a37c2c9ace7a264be483ebcb7b13b8cd9f0be
halfwit <michaelmisch1985@gmail.com>
2021-03-23 19:09:05 +0000

    Stash these changes for later

--- wiki/config/config.php
+++ wiki/config/config.php
@@ -1,10 +1,9 @@
 <?php
 
 // Copy this file to "config.php" and change the following values
-
-define("MYSQL_USER", getenv("DB_USERNAME"));
-define("MYSQL_PASSWORD", getenv("DB_PASSWORD"));
-define("MYSQL_DATABASE", getenv("DB_DATABASE"));
+define("MYSQL_USER", getenv('DB_USERNAME'));
+define("MYSQL_PASSWORD", getenv('DB_PASSWORD'));
+define("MYSQL_DATABASE", getenv('DB_DATABASE'));
 
 // Password used to avoid captchas via the login page
 define("LOGIN_PASSWORD", "password"); 

8f25d30d15e090e17880a947ddf7b1b3dde3017f
halfwit <michaelmisch1985@gmail.com>
2024-09-19 17:20:55 -0700

    Small typo

--- wiki/src/markup/embed.php
+++ wiki/src/markup/embed.php
@@ -50,7 +50,7 @@ function embed_html5_video($input, $options = array())
     }
 
     $attributes = implode(' ', $attributes);
-    if(preg_match("^/https?/", $input))
+    if(preg_match("/^https?/", $input))
 	return "<video $attributes><source src='{$input}'></video>";
     return "<video $attributes><source src='/{$input}'></video>";
 }
